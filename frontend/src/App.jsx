815248a40ceb322c884f11e8b68d4d9a02eb147b
chakribontha <bonthachakri1@gmail.com>
2024-02-22 14:20:17 +0530

    complete project after backend deoplement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import "./App.css";
 import Forms from "./components/Forms";
 import RoomPage from "./pages/RoomPage";
 
-const server = "http://localhost:500";
+const server = "https://bakendofsketch-2.onrender.com";
 const connectionOptions = {
   "force new connection": true,
   reconnectionAttempts: "Infinity",
